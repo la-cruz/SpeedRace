885641f8dea8593435b1170798dae6de3b0e3fcd
Mickael Alves <0mickael.alves0@gmail.com>
2020-02-11 16:31:01 +0100

    Adding setup Spring boot

---
+++ SpringApp/src/test/java/m1if13/SpringApp/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package m1if13.SpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
