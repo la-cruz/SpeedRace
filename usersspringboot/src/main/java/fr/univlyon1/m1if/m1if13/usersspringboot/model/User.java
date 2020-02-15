93d4c8f4573ba223aed5c4f89413264022889813
AUDART <lucas.audart@etu.univ-lyon1.fr>
2020-02-15 08:42:51 +0100

    UserDao creation

--- usersspringboot/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/model/User.java
+++ usersspringboot/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/model/User.java
@@ -4,13 +4,11 @@ import javax.naming.AuthenticationException;
 
 public class User {
     private String login, password;
-    // Permet d'invalider une connexion même si le token est toujours valide
     private boolean connected;
 
     public User(String login, String password) {
         this.login = login;
         this.password = password;
-        // Quand un utilisateur est créé, il n'est pas connecté
         this.connected = false;
     }
 
