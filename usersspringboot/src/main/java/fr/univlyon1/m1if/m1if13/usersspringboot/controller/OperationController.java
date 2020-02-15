6fe13f5597d87985fe57c0f4ddba256855c7cc4b
Mickael Alves <0mickael.alves0@gmail.com>
2020-02-15 15:35:01 +0100

    Test deploy on VM

--- usersspringboot/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/controller/OperationController.java
+++ usersspringboot/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/controller/OperationController.java
@@ -17,18 +17,18 @@ public class OperationController {
      * @param password Le password à vérifier.
      * @return Une ResponseEntity avec le JWT dans le header "Authentication" si le login s'est bien passé, et le code de statut approprié (204, 401 ou 404).
      */
-    @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestParam("login") String login, @RequestParam("password") String password, @RequestHeader("Origin") String origin) {
-        // TODO
-    }
+    // @PostMapping("/login")
+    // public ResponseEntity<Void> login(@RequestParam("login") String login, @RequestParam("password") String password, @RequestHeader("Origin") String origin) {
+    //     // TODO
+    // }
 
     /**
      * Réalise la déconnexion
      */
-    @PostMapping("/logout")
-    public ResponseEntity<Void> logout() {
-        // TODO
-    }
+    // @PostMapping("/logout")
+    // public ResponseEntity<Void> logout() {
+    //     // TODO
+    // }
 
     /**
      * Méthode destinée au serveur Node pour valider l'authentification d'un utilisateur.
@@ -36,8 +36,8 @@ public class OperationController {
      * @param origin L'origine de la requête (pour la comparer avec celle du client, stockée dans le token JWT)
      * @return Une réponse vide avec un code de statut approprié (204, 400, 401).
      */
-    @GetMapping("/authenticate")
-    public ResponseEntity<Void> authenticate(@RequestParam("token") String token, @RequestParam("origin") String origin) {
-        // TODO
-    }
+    // @GetMapping("/authenticate")
+    // public ResponseEntity<Void> authenticate(@RequestParam("token") String token, @RequestParam("origin") String origin) {
+    //     // TODO
+    // }
 }
