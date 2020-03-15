15b94e57b98bf389c9c1b27d442928e9bcbcac9f
Mickael Alves <0mickael.alves0@gmail.com>
2020-03-15 10:54:26 +0100

    Fix new TP3

--- app/server/server.js
+++ app/server/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.use('/static', express.static('public'));
+app.use('/static', express.static('../client/public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
