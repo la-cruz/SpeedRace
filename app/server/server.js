38fdb3511166c074b855261869b44eea21e4dc3c
AUDART <lucas.audart@etu.univ-lyon1.fr>
2020-03-16 17:02:01 +0100

    Changing port to 3000

--- app/server/server.js
+++ app/server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5500
+const port = 3000
 const apiRouter = require("./routes/api")
 const adminRouter = require("./routes/admin")
 
