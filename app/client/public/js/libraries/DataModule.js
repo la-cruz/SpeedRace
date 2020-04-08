9c06855e5d1cb813dae4236db0a6dda01add4836
AUDART <lucas.audart@etu.univ-lyon1.fr>
2020-04-08 12:52:17 +0200

    Fixing ajax url

--- app/client/public/js/libraries/DataModule.js
+++ app/client/public/js/libraries/DataModule.js
@@ -1,34 +1,34 @@
 import AjaxRequest from './AjaxRequest'
 
 function trophies(login) {
-    return AjaxRequest.getData('http://192.168.75.18:3376/api/resources/' + login + '/trophies')
+    return AjaxRequest.getData('http://192.168.75.28:3376/api/resources/' + login + '/trophies')
         .then((response) => {
             return response.json()
         })
 }
 
 function stats(login) {
-    return AjaxRequest.getData('http://192.168.75.18:3376/api/resources/' + login)
+    return AjaxRequest.getData('http://192.168.75.28:3376/api/resources/' + login)
         .then((response) => {
             return response.json()
         })
 }
 
 function list() {
-    return AjaxRequest.getData('http://192.168.75.18:3376/api/resources')
+    return AjaxRequest.getData('http://192.168.75.28:3376/api/resources')
         .then((response) => {
             return response.json()
         })
 }
 
 function changePosition(login, lat, lon) {
-    return AjaxRequest.putData('http://192.168.75.18:3376/api/resources/' + login + '/position', {
+    return AjaxRequest.putData('http://192.168.75.28:3376/api/resources/' + login + '/position', {
         position: [lat, lon]
     })
 }
 
 function changeImage(login, url) {
-    return AjaxRequest.putData('http://192.168.75.18:3376/api/resources/' + login + '/image', {
+    return AjaxRequest.putData('http://192.168.75.28:3376/api/resources/' + login + '/image', {
         url: url
     })
 }
--- app/client/public/js/libraries/GameHandler.js
+++ app/client/public/js/libraries/GameHandler.js
@@ -1,20 +1,20 @@
 import AjaxRequest from './AjaxRequest'
 
 function start(name) {
-    return AjaxRequest.putData('http://localhost:3000/admin/start', {
+    return AjaxRequest.putData('http://192.168.75.28:3376/admin/start', {
         name: name
     })
 }
 
 function stop() {
-    return AjaxRequest.putData('http://localhost:3000/admin/stop')
+    return AjaxRequest.putData('http://192.168.75.28:3376/admin/stop')
         .then((response) => {
             return response.json()
         })
 }
 
 function status() {
-    return AjaxRequest.getData('http://localhost:3000/admin/status')
+    return AjaxRequest.getData('http://192.168.75.28:3376/admin/status')
         .then((response) => {
             return response.json()
         })
