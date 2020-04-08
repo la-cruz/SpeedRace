511edaaaa9e6c5debab81fb68b49279058eb9637
AUDART <lucas.audart@etu.univ-lyon1.fr>
2020-04-08 10:31:43 +0200

    Changing ajax request url

--- app/client/public/js/libraries/DataModule.js
+++ app/client/public/js/libraries/DataModule.js
@@ -1,35 +1,34 @@
 import AjaxRequest from './AjaxRequest'
 
 function trophies(login) {
-    return AjaxRequest.getData('http://localhost:3000/api/resources/' + login + '/trophies')
+    return AjaxRequest.getData('http://192.168.75.18:3376/api/resources/' + login + '/trophies')
         .then((response) => {
             return response.json()
         })
 }
 
 function stats(login) {
-    return AjaxRequest.getData('http://localhost:3000/api/resources/' + login)
+    return AjaxRequest.getData('http://192.168.75.18:3376/api/resources/' + login)
         .then((response) => {
-            console.log(response)
             return response.json()
         })
 }
 
 function list() {
-    return AjaxRequest.getData('http://localhost:3000/api/resources')
+    return AjaxRequest.getData('http://192.168.75.18:3376/api/resources')
         .then((response) => {
             return response.json()
         })
 }
 
 function changePosition(login, lat, lon) {
-    return AjaxRequest.putData('http://localhost:3000/api/resources/' + login + '/position', {
+    return AjaxRequest.putData('http://192.168.75.18:3376/api/resources/' + login + '/position', {
         position: [lat, lon]
     })
 }
 
 function changeImage(login, url) {
-    return AjaxRequest.putData('http://localhost:3000/api/resources/' + login + '/image', {
+    return AjaxRequest.putData('http://192.168.75.18:3376/api/resources/' + login + '/image', {
         url: url
     })
 }
