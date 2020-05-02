66412234d206bf4fdbb83bf93bd9057fdc22eeb5
Mickael Alves <0mickael.alves0@gmail.com>
2020-05-03 00:27:48 +0200

    statistique page css

--- app/client/public/js/components/Stats.vue
+++ app/client/public/js/components/Stats.vue
@@ -1,18 +1,22 @@
 <template>
-    <section class="statistique">
-        <h1>Statistique de {{ login }}</h1>
-        <ul>
-            <li><span>Role :</span> {{ stats.role }}</li>
-            <li><span>Position :</span> {{ stats.position }}</li>
-            <li><span>Time to live :</span> {{ stats.ttl }}</li>
-            <li><span>Image :</span> {{ stats.url }}</li>
-            <li><span>Flouté ? :</span> {{ stats.blurred }}</li>
-            <li><span>Status :</span> {{ stats.status }}</li>
-        </ul>
-        <form @submit.prevent>
-            <label for="newImage">Nouvelle image :</label><input type="text" name="newImage" v-model="url" />
-            <button type="submit" @click.prevent="changeImage">Changer</button>
-        </form>
+    <section class="page statistique">
+        <div class="container-statistique">
+            <h1>Statistique de {{ login }}</h1>
+            <hr>
+            <ul>
+                <li><span>Role :</span> {{ stats.role }}</li>
+                <li><span>Position :</span> {{ stats.position }}</li>
+                <li><span>Time to live :</span> {{ stats.ttl }}</li>
+                <li><span>Image :</span> {{ stats.url }}</li>
+                <li><span>Flouté ? :</span> {{ stats.blurred }}</li>
+                <li><span>Status :</span> {{ stats.status }}</li>
+            </ul>
+            <hr>
+            <form @submit.prevent>
+                <label for="newImage">Nouvelle image :</label><input type="text" name="newImage" v-model="url" />
+                <button type="submit" @click.prevent="changeImage">Changer</button>
+            </form>
+        </div>
     </section>
 </template>
 
@@ -56,39 +60,82 @@
 
 <style lang="scss">
     .statistique {
-        padding-top: 10rem;
+        height: calc(100vh - 5rem);
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-        h1 {
-            margin-bottom: 3rem;
-        }
+        .container-statistique {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            background-color: white;
+            border-radius: 10px;
+            padding: 2rem 3rem;
+            box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
 
-        ul {
-            width: 70%;
-            margin: auto;
+            h1 {
+                color: rgb(56, 56, 56);
+                font-size: 1.2rem;
+                text-transform: uppercase;
+                font-weight: bold;
+                text-align: center;
+            }
 
-            li {
-                position: relative;
+            hr {
+                width: 100%;
+                height: 4px;
+                border: none;
+                margin: 2rem 0;
+                background-color: #FFCD00;
+                text-align: center;
+            }
+    
+            ul {
                 display: flex;
-                justify-content: space-between;
-                margin: 1rem 0;
-
-                span {
-                    color: yellow;
-                    font-weight: bold;
+                flex-direction: column;
+                justify-content: center;
+    
+                li {
+                    margin: 1rem 0;
+                    color: rgb(56, 56, 56);
+                    font-size: 1.1rem;
+    
+                    span {
+                        color: #FFCD00;
+                        font-weight: bold;
+                        padding-right: 1rem;
+                        font-size: 1.3rem;
+                    }
                 }
             }
-        }
+    
+            form {
+                width: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: space-evenly;
 
-        form {
-            margin-top: 3rem;
-            width: 70%;
-            margin: 0 auto;
-            display: flex;
-            justify-content: space-evenly;
-
-            button {
-                color: black;
-                margin-left: 1rem;
+                label {
+                    color: rgb(56, 56, 56);
+                    margin-right: 1rem;
+                    font-size: 1.1rem;
+                    text-transform: uppercase;
+                    font-weight: bold;
+                }
+    
+                button {
+                    background-color: #FFCD00;
+                    width: max-content;
+                    font-size: 1.2rem;
+                    color: white;
+                    border: none;
+                    border-radius: 50px;
+                    text-transform: uppercase;
+                    padding: 0.8rem 1rem;
+                    margin-left: 1rem;
+                    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.5);
+                }
             }
         }
     }
