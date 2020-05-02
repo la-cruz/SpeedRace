9f0f3d0e484f86d64b6f99719797c9685f11abb3
AUDART <lucas.audart@etu.univ-lyon1.fr>
2020-05-02 18:25:50 +0200

    Adding news cors restriction

--- users/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/controller/UserRestController.java
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/controller/UserRestController.java
@@ -51,7 +51,7 @@ public class UserRestController {
 			@ApiResponse(responseCode = "204", description = "There is no user")
 		}
 	)
-	@CrossOrigin(origins = {"http://localhost", "http://192.168.75.28", "https://192.168.75.28"})
+	@CrossOrigin(origins = {"http://localhost", "http://localhost:3376", "http://192.168.75.28", "https://192.168.75.28"})
 	@GetMapping(path = "/users", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<Set<String>> getUsers(
 		@Parameter(description = "The host uri", in = ParameterIn.HEADER) @RequestHeader("host") String host
